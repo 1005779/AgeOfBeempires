17552016e14d96c30cd4aead00a8b73d9d3a0809
1005779 <t.quick@live.com.au>
2016-08-02 23:43:31 +1000

    First Push

---
+++ Assets/Scripts/QueenHive.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QueenHive : MonoBehaviour {
+
+    // Variables for Currency conversion
+    private int pollen;
+    private int honey;
+
+    // Variables for Build Materials
+    private int workers;
+    private int wax;
+
+    // Build Costs
+    public int droneHive = 2;
+
+    // Timers Variables
+    public int honeyTime = 2;
+
+    // Prefabs to instantiate
+    public GameObject DroneHive;
+    public GameObject hiveSpot;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Start Values so player can play straight away
+        honey = 8;
+        workers = 2;
+        wax = 8;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DropOff(int nectar)
+    {
+        pollen = pollen + (nectar / 2);
+    }
+
+    public void Food()
+    {
+        new WaitForSeconds(honeyTime);
+
+        if (pollen >= 2)
+        {
+            pollen -= 2;
+            honey += 2;
+        }
+    }
+
+    public void Currency()
+    {       
+        if (honey >= 2)
+            {
+                wax += honey;
+            }               
+    }
+
+    public void BuildDroneHive()
+    {
+        if (Input.GetKeyDown("space") && wax >= droneHive)
+        {
+            wax -= droneHive;
+           // Instantiate(DroneHive,);
+        }
+    }
+}
